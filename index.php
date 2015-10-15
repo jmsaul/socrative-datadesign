f0cd9956d428e8ed19071a23c874521859688ffc
Jeff Saul <scaleup13@gmail.com>
2015-10-15 06:54:17 -0600

    Web page for Deep Dive Coding Data Design Project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Socrative Quiz Writing</title>
+	</head>
+	<body>
+		// This is Phase 1 of the Data Design project for Deep Dive Coding
+		<h1>Jeff - Socrative Quiz Writing - Data Design Project</h1>
+		<h2>Phase 1</h2> // The first two lines are the title of the project
+		<hr>
+		<h3>Define the typical Persona</h3>
+		// This is a description of one of the end users
+		<p>Pesona 1 is Mr. John Smith.  John Smith is an APS high school history teacher who is somewhat tech savvy but not an expert on education technology. He knows enough to look for a help button or for FAQ page when he has questions.  He does not have a lot of time to write and grade quizzes.  He does have at least one computer for each student.  Communication for the Quiz creation stage is one-to-one. This is his first time using socrative.com to give a reading quiz.  Since socrative does not work well on mobile devices and there is a lot of writing, Mr. Smith is writing the quiz on his laptop at his desk in his classroom.</p>
+		<hr>
+		<h3>Main Use Case 1</h3>
+		<h4>Main Goal</h4>
+		//  This is a brief description of what the end user wants to do, the end user's goal
+		<p>Mr. Smith is writing a  three-question quiz for his class on his socrative.com account to assess his students' understanding of today's reading assignment.  The quiz will be used at the start of his next class. Based on the questions Mr. Smith has prepared, there will be one True/False question, one multiple-choice question, and one short answerwritten the three quesitons for his quiz ahead of time.  The first question is a true/false focusing on a particular concept or vocabulary word.  It is easy to help students feel confident and reduce student anxiety.  The second question is a more challenging, conceptual multiple-choice question to assess student understanding applying that concept.  The third question is to get students either to ask their most pressing questions or describe the most interesting or surprising thing they found in the reading.  To encourage students to think about their experiences with the reading assignment, this question is open-ended short answer.</p>
+		<h4>Defining steps arriving at that goal</h4>
+		//  This is a step-by-step procedure for the user to arrive at his end goal
+		<p></p>
+	</body>
+
+</html>
